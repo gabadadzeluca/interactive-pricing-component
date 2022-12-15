b42f27bdc8e4466eb64799fd3c48e6136d34e1be
Luca <gabadadzeluca@gmail.com>
2022-12-15 14:30:58 +0400

    finish project

---
+++ README.md
@@ -0,0 +1,51 @@
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+- [Author](#author)
+
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Use the slider and toggle to see prices for different page view numbers
+
+### Screenshot
+![screenshot](screenshot.png)
+
+### Links
+
+- Solution URL: [GitHub](https://github.com/gabadadzeluca/interactive-pricing-component)
+- Live Site URL: [Live URL]()
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+
+### What I learned
+
+Learned about grid-row and grid-column properties in css, used switch/case logic in js and made an interactive range input model for the first time.
+
+
+## Author
+
+- LinkedIn- [Luca Gabadadze](https://www.linkedin.com/in/luca-gabadadze-6068b324a/)
+- GitHub - [gabadadzeluca](https://www.frontendmentor.io/profile/yourusername)
+
+
--- script.js
+++ script.js
@@ -1,27 +1,38 @@
 let yearlySub = false;
 
-
 const total = document.getElementById('total');
 const billCheck = document.getElementById('bill-check');
 billCheck.addEventListener('change', displayTotal);
 const slider = document.getElementById('slider');
 const period = document.getElementById('period');
 //pageviews 
-
 const pageviews = document.getElementById('pageviews');
-slider.addEventListener('change', function(){
+
+slider.addEventListener('input', function(){
     displayTotal();
+    sliderFunc();
 });
 
+function sliderFunc(){
+    // console.log("background: ",slider.style.background);
+    let percent = Math.min(100, (slider.value / slider.max) * 100);
+    // console.log("percent: ",percent,"max: ", slider.max);
+    // slider.style.background = `linear-gradient(to right , var(--soft-cyan) ${(percent)}%, var(--slider-blue) ${(percent)}%)`;
+    // if(percent < 34){
+    //     slider.style.background = 'var(--slider-blue)';
+    // }
+}
 // show the starting price
 displayTotal();
-
-
 
 function displayTotal(){
     getBilling();
     let x = yearlySub ? 9 : 1; 
-    total.innerHTML = '$' + parseInt(slider.value * x);
+    if(yearlySub){
+        total.innerHTML = '$' + parseInt(slider.value*3/4 * x).toFixed(2);
+    }else{
+        total.innerHTML = '$' + parseInt(slider.value * x).toFixed(2);
+    }
 }
 
 function getBilling(){
@@ -35,29 +46,40 @@ function getBilling(){
         period.innerHTML = '/month';
         yearlySub = false;
     }
-    displayViews();
+    displayValues();
 }
 
-// display pageviews
-function displayViews(){
+// display pageviews and slider background gradient
+function displayValues(){
     // value = parseInt(slider.value);
     let x;
+    let val;
     switch(parseInt(slider.value)){
         case 8:
-            x = 10
+            val = 0;
+            x = 10;
             break;
         case 12:
+            val = 25;
             x = 50;
             break;
         case 16:
+            val = 50;
             x = 100;
             break;
         case 20:
-            x = 150;
+            val = 75;
+            x = 500;
             break;
         case 24:
-            x = 200;
+            val = 100;
+            x = 1;
             break;
     }
-    pageviews.innerHTML = `${x} K`
+    pageviews.innerHTML = `${x} K`;
+    slider.style.background = `linear-gradient(to right , var(--soft-cyan) ${(val)}%, var(--slider-blue) ${(val)}%)`;
+
+    if(x==1){
+        pageviews.innerHTML = `${x} M`
+    }
 }
